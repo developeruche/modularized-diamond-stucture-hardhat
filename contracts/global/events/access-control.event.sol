932f09f1d2f31690f662270e93f3d96575a39d88
Nwele Uchenna <nweleuchenna2004@gmail.com>
2023-03-03 02:49:13 +0100

    developeruche: installed access control

--- contracts/global/events/access-control.event.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-
-
-/// @notice this library holds all the errors related to the Access control module 
-library AccessControlEvent {
-  event SET_UP(address superUser);
-}
--- contracts/global/events/bank.event.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-
-
-/// @notice this library holds all the errors related to the Access control module 
-library AccessControlEvent {
-  event DEPOSITED(address from, uint256 amount);
-}
