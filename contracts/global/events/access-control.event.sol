3ac3da5205a19925d87075e0ced3266ab9b9f0b0
Nwele Uchenna <nweleuchenna2004@gmail.com>
2023-03-03 02:44:41 +0100

    developeruche: installed event sub-module

---
+++ contracts/global/events/access-control.event.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+
+/// @notice this library holds all the errors related to the Access control module 
+library AccessControlEvent {
+  event SET_UP(address superUser);
+}
