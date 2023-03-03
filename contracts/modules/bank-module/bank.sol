e25e46518e201656f981e17d0a2c93e881f76d43
Nwele Uchenna <nweleuchenna2004@gmail.com>
2023-03-03 03:53:48 +0100

    developeruche: installed bank enrty point

--- contracts/modules/bank-module/bank.sol
+++ contracts/modules/bank-module/bank.sol
@@ -1,20 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import {BankController as Controller} from "./controllers/bank.controller.sol";
-
+import {BankProvider as provider} from "./providers/bank.provider.sol";
 
 contract Bank is Controller{
     function deposit() 
         external
         payable 
     {
-
+        provider.deposit();
     }
 
     function withdraw()
         external 
     {
+        provider.withdraw();
+    }
 
+    function pause(
+        bool _status
+    )
+        external 
+    {
+        provider.pause(_status);
     }
 
     function balance(
@@ -26,6 +34,8 @@ contract Bank is Controller{
             uint256 bal_
         )
     {
-        bal_
+        bal_ = provider.bal(_addr);
     }
+
+
 }
